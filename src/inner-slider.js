ea3de85a9e33e88314224498ddc13c4fb0e9cfba
Chidanandan P <chidanandan.p@valtech.co.in>
2018-03-14 15:17:52 +0530

    testingFinal001

--- src/inner-slider.js
+++ src/inner-slider.js
@@ -41,6 +41,7 @@ export var InnerSlider = React.createClass({
     for (var i = 0; i < React.Children.count(this.props.children); i++) {
       if (i >= this.state.currentSlide && i < this.state.currentSlide + this.props.slidesToShow) {
         lazyLoadedList.push(i);
+        lazyLoadedList.push(i+1);
       }
     }
 
